5b1156498768be319022d8fc44722d56b9446944
Ole Kristian Aune <okaune@outlook.com>
2015-09-18 16:49:41 +0200

    Startet på kjørelogikk-test

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import lejos.hardware.port.Port;
 import lejos.hardware.ev3.EV3;
 import lejos.robotics.SampleProvider;
 import lejos.hardware.sensor.*;
+import lejos.robotics.navigation.DifferentialPilot;
 
 
 public class Main{
@@ -24,15 +25,36 @@ public class Main{
 		SampleProvider fargeLeser2 = fargesensor2.getMode("RGB");  // svart = 0.01..
 		float[] fargeSample2 = new float[fargeLeser2.sampleSize()];  // tabell som innholder avlest verdi
 
-	    double gray = 0;
+		// Registrerer differentialPilot
+		DifferentialPilot pilot = new DifferentialPilot(56, 120, Motor.B, Motor.C, false);
+		pilot.setTravelSpeed(80);
+		pilot.setRotateSpeed(150);
+
+	    int color1 = 0;
+		int color2 = 0;
 	    for (int i = 0; i<100; i++){
 	    	fargeLeser1.fetchSample(fargeSample1, 0);
 			fargeLeser2.fetchSample(fargeSample2, 0);
-	    	gray = (fargeSample1[0] + fargeSample2[0]) / 2 * 100;
-			System.out.println(String.format("Venstre side: %.2f", fargeSample1[0] * 100));
-			System.out.println(String.format("Høyre side: %.2f", fargeSample2[0] * 100));
-			System.out.println(String.format("Gjennomsnitt: %.2f", gray));
-			Thread.sleep(1500);
+	    	color1 += fargeSample1[0] * 100;
+			color2 += fargeSample2[0] * 100;
 	    }
+		color1 /= 100;
+		color2 /= 100;
+
+		while (true) {
+			fargeLeser1.fetchSample(fargeSample1, 0);
+			fargeLeser2.fetchSample(fargeSample2, 0);
+			if (fargeSample2[0] * 100 < color2) {
+				System.out.println("Høyre!");
+				pilot.rotateRight();
+			} else if (fargeSample1[0] * 100 < color1) {
+				System.out.println("Venstre!");
+				pilot.rotateLeft();
+			} else {
+				System.out.println("Fremover!");
+				pilot.forward();
+			}
+			Thread.sleep(200);
+		}
 	}
 }
